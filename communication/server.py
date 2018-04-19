379495db0df8c814ffa3293a9fb29c46594e41fc
Henrique José Dalla Corte <riquejdc@gmail.com>
2018-04-18 22:45:17 -0300

    Writh in File and Lock for this

--- communication/server.py
+++ communication/server.py
@@ -1,5 +1,8 @@
 import socket
+import threading
 import time
+
+tLock = threading.Lock()
 
 host = '127.0.0.1'
 port = 5000
@@ -9,6 +12,8 @@ clients = []
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((host, port))
 s.setblocking(0)
+arquivo = open('conversa.txt', 'r')
+conteudo = arquivo.readlines()
 
 quitting = False
 print("Server Started.")
@@ -16,6 +21,12 @@ while not quitting:
     try:
         data, addr = s.recvfrom(1024)
         decoded_data = data.decode('utf-8')
+        tLock.acquire()
+        conteudo.append(decoded_data +'\n')
+        arquivo = open('conversa.txt', 'w')
+        arquivo.writelines(conteudo)
+        arquivo.close()
+        tLock.release()
         if ("Quit") in str(decoded_data):
             quitting = True
 
@@ -23,10 +34,12 @@ while not quitting:
             print("Client ", addr, "has connected.")
             clients.append(addr)
 
-        print(time.ctime(time.time()), addr, ": :", decoded_data)
+        print(time.ctime(time.time()), addr, ":", decoded_data)
+        time.sleep(0.2)
         for client in clients:
-            s.sendto(data, client)
+            if addr != client:
+                s.sendto(data, client)
 
     except:
-        pass
+        pass    
 s.close()
