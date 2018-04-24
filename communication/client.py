a4cbbc570e76e8ce0695330f3db6e1f7a6c5c6da
Forbrig <vitorforbrig@gmail.com>
2018-04-24 19:21:04 -0300

    hora do servidor e do cliente quando envia a mensagem

--- communication/client.py
+++ communication/client.py
@@ -12,16 +12,16 @@ def receving(name, sock):
             while True:
                 data, addr = sock.recvfrom(1024)
                 decoded_data = data.decode('utf-8')
-                print(decoded_data+'\n')
+                print(decoded_data)
         except:
             pass
         finally:
             tLock.release()
 
-def past_conv():
-    arquivo = open('conversa.txt', 'r')
-    conteudo = arquivo.readlines()
-    print(conteudo)
+# def past_conv():
+#     arquivo = open('conversa.txt', 'r')
+#     conteudo = arquivo.readlines()
+#     print(conteudo)
 
 
 host = '127.0.0.1'
@@ -37,30 +37,32 @@ receivingThread = threading.Thread(target = receving, args = ("RecvThread", s))
 receivingThread.start()
 
 #past_conv()
-messages = []
-print("Press Enter Before Send Message, message send only with ->")
+#messages = []
+print("Press enter to send the message.")
 alias = input("Name: ")
-message = 'Connect'
-message = (alias + ": " + message + "\n"  + time.ctime(time.time()))
+message = alias
 message = message.encode('utf-8')
+# connect message
 s.sendto(message, server)
-time.sleep(0.3)
 
-message = input(alias + "-> ")
-messages.append(message)
+message = input()
+#messages.append(message)
 
 while (message != 'Quit'):
     if (message != ''):
-        message = (alias + ": " + message)
+        message = (alias + ": " + message + " Date: " + time.ctime(time.time()))
+        #print(message)
         message = message.encode('utf-8')
-        s.sendto(message, server)
-    message = input(alias + "-> ")
-    time.sleep(0.3)
 
-message = 'Desconnect'
+        time.sleep(2)
+        s.sendto(message, server)
+    message = input()
+    #time.sleep(0.3)
+
+message = 'Desconnecting from server'
 message = (alias + ": " + message)
 message = message.encode('utf-8')
 s.sendto(message, server)
-shudown = True
+shutdown = True
 receivingThread.join()
 s.close()
--- communication/server.py
+++ communication/server.py
@@ -12,11 +12,11 @@ clients = []
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((host, port))
 s.setblocking(0)
-arquivo = open('conversa.txt', 'r')
+arquivo = open('conversa.txt', 'w+')
 conteudo = arquivo.readlines()
 
 quitting = False
-print("Server Started.")
+print("Server Started at port:", port, "and address:", host, ".")
 while not quitting:
     try:
         data, addr = s.recvfrom(1024)
@@ -31,20 +31,33 @@ while not quitting:
             quitting = True
 
         if addr not in clients:
-            print("Client ", addr, "has connected.")
+            print("Client", addr, "has connected as", decoded_data.split(' ')[0].replace(":", ""), "at", time.ctime(time.time()))
             clients.append(addr)
+        else:
+            m = decoded_data.split(' ')
+            for i, token in enumerate(m[:]):
+                if token == 'Date:':
+                    break
+            data = m[i+4:i+5]
+            #print(data)
+            data = str(data).replace(":", " ").replace("[", "").replace("]", "").replace("\'", "")
+            data = data.split(' ')
+            segundos_cliente = (int(data[0]))*60*60 + (int(data[1]))*60 + (int(data[2])) #segundos da mensagem do cliente quando chega no servidor
+            print(segundos_cliente)
 
-        #data_message,date_message = decoded_data.split('|',1)
-        #if(time.ctime(time.time()) > decoded_data.split("|")[1])
-        print(time.ctime(time.time()))
-        #else
-        #print(date_message)
-        print(addr, ":", decoded_data.split("\n")[0])
-        time.sleep(0.2)
-        for client in clients:
-            if addr != client:
-                s.sendto(data, client)
+            segundos_servidor = time.ctime(time.time())
+            segundos_servidor = (segundos_servidor).split(' ')[3:4]
+            segundos_servidor = str(segundos_servidor).replace(":", " ").replace("[", "").replace("]", "").replace("\'", "")
+            segundos_servidor = segundos_servidor.split(' ')
+            segundos_servidor = (int(segundos_servidor[0]))*60*60 + (int(segundos_servidor[1]))*60 + (int(segundos_servidor[2])) #segundos do servidor quando chega a mensagem do cliente
 
+            print(segundos_servidor)
+
+
+            print(time.ctime(time.time()), "|", decoded_data.split("\n")[0])
+            for client in clients:
+                if addr != client:
+                    s.sendto(data, client)
     except:
         pass
 s.close()
