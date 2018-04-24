aaef7bd058136ea7fcf757d5a2aeb8e2af94b8a7
Henrique José Dalla Corte <riquejdc@gmail.com>
2018-04-24 20:17:05 -0300

    Update server.py

--- communication/server.py
+++ communication/server.py
@@ -40,14 +40,14 @@ while not quitting:
             for i, token in enumerate(m[:]):
                 if token == 'Date:':
                     break
-                print(message_envio)
+                #print(message_envio)
                 message_envio = (message_envio + token + (' '))
 
             segundos_cliente = m[i+4:i+5]
             segundos_cliente = str(segundos_cliente).replace(":", " ").replace("[", "").replace("]", "").replace("\'", "")
             segundos_cliente = segundos_cliente.split(' ')
             segundos_cliente = (int(segundos_cliente[0]))*60*60 + (int(segundos_cliente[1]))*60 + (int(segundos_cliente[2])) #segundos da mensagem do cliente quando chega no servidor
-            print(segundos_cliente)
+            #print(segundos_cliente)
             ################################
             segundos_servidor = time.ctime(time.time())
             message_envio = (message_envio + ('Date: ') + segundos_servidor)
@@ -56,7 +56,7 @@ while not quitting:
             segundos_servidor = segundos_servidor.split(' ')
             segundos_servidor = (int(segundos_servidor[0]))*60*60 + (int(segundos_servidor[1]))*60 + (int(segundos_servidor[2])) #segundos do servidor quando chega a mensagem do cliente
 
-            print(segundos_servidor)
+            #print(segundos_servidor)
 
             print(time.ctime(time.time()), "|", decoded_data.split("\n")[0])
             for client in clients:
